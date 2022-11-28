2fefc052377b16e2a0217367e99d77b21a43113c
Grasiela Texeira <grasy.mst@gmail.com>
2022-11-28 10:36:41 -0300

    Create of factories ( user, campaign and member)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+<<<<<<< HEAD
+=======
+#Dir[Rails.root.join('spec/support/**/*.rb')].each ( |f| require f )
+>>>>>>> service
 
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
